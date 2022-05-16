0cb72daf75fb722be34864524a41c92e30b2b5f8
chendaofu <tofuchen@126.com>
2022-05-16 20:42:23 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,6 @@ module.exports = function override(config, env) {
     if (env === "production") {
         config.output.publicPath = "/pdf-viewer/build/";
     }
+    // console.log(config.module.rules[1].oneOf[7]);
     return config;
 };
